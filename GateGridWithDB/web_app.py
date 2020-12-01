f0adbbaf6f50c307222f859e6801e9f788a8ad6d
TitusWongCN <wzhwno1@163.com>
2020-12-01 23:57:42 +0800

    fix buy-sell desc bug & optimize chart info

--- GateGridWithDB/web_app.py
+++ GateGridWithDB/web_app.py
@@ -4,6 +4,7 @@ from flask import Flask, url_for, redirect
 from db_model import *
 import time
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import annotate
 
 app = Flask('GridInfo')
 host = '119.3.78.178'
@@ -33,7 +34,7 @@ def accountinfo():
 def operinfo():
     last_oper = oper_his.get(oper_his.id == oper_his.select().count())
     last_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(int(last_oper.timestamp)))
-    oper_type = '买入' if last_oper.cur_price == 'buy' else '卖出'
+    oper_type = '买入' if last_oper.type == 'buy' else '卖出'
     oper_desc = [
         f'最近一次操作是在: {last_time}',
         f'在{last_oper.token_name}价格为{last_oper.cur_price} USDT时，{oper_type} {last_oper.trade_count} USDT',
@@ -49,12 +50,50 @@ def gridchart():
              for account_data in account_datas]
     indexes = [int(account_data.id) for account_data in account_datas]
     prices = [float(account_data.cur_price) for account_data in account_datas]
-    prices = [price/max(prices) for price in prices]
+    norm_prices = [price/max(prices) for price in prices]
+    min_price_index = norm_prices.index(min(norm_prices))
+    max_price_index = norm_prices.index(max(norm_prices))
     balances = [float(account_data.balance) for account_data in account_datas]
-    balances = [balance / max(balances) for balance in balances]
+    norm_balances = [balance / max(balances) for balance in balances]
+    min_balance_index = norm_balances.index(min(norm_balances))
+    max_balance_index = norm_balances.index(max(norm_balances))
     fig, ax = plt.subplots(1, 1)
-    plt.plot(indexes, prices, c='blue', label='EOS')
-    plt.plot(indexes, balances, c='red', label='GRID')
+    plt.plot(indexes, norm_prices, c='blue', label='EOS')
+    annotate(f"{'%.4f' % prices[0]}",
+             xy=(0, norm_prices[0]), xycoords='data',
+             xytext=(15, -20), textcoords='offset points', fontsize=10, color='purple',
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    annotate(f"{'%.4f' % prices[min_price_index]}",
+             xy=(min_price_index, norm_prices[min_price_index]), xycoords='data',
+             xytext=(-50, 15), textcoords='offset points', fontsize=10, color='purple',
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    annotate(f"{'%.4f' % prices[-1]}",
+             xy=(len(norm_prices) - 1, norm_prices[-1]), xycoords='data',
+             xytext=(5, 30), textcoords='offset points', fontsize=10, color='purple',
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    if max_price_index not in [0, min_price_index, len(norm_prices) - 1]:
+        annotate(f"{'%.4f' % prices[max_price_index]}",
+                 xy=(max_price_index, norm_prices[max_price_index]), xycoords='data',
+                 xytext=(0, 30), textcoords='offset points', fontsize=10, color='purple',
+                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    plt.plot(indexes, norm_balances, c='red', label='GRID')
+    annotate(f"{'%.4f' % balances[0]}",
+             xy=(0, norm_balances[0]), xycoords='data',
+             xytext=(15, -45), textcoords='offset points', fontsize=10, color='purple',
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    annotate(f"{'%.4f' % balances[min_balance_index]}",
+             xy=(min_balance_index, norm_balances[min_balance_index]), xycoords='data',
+             xytext=(-50, 15), textcoords='offset points', fontsize=10, color='purple',
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    annotate(f"{'%.4f' % balances[-1]}",
+             xy=(len(norm_balances) - 1, norm_balances[-1]), xycoords='data',
+             xytext=(5, 30), textcoords='offset points', fontsize=10, color='purple',
+             arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    if max_price_index not in [0, min_price_index, len(norm_prices) - 1]:
+        annotate(f"{'%.4f' % balances[max_price_index]}",
+                 xy=(max_price_index, norm_balances[max_price_index]), xycoords='data',
+                 xytext=(0, 30), textcoords='offset points', fontsize=10, color='purple',
+                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
     # plt.plot(prices, c='blue', label='EOS')
     # plt.plot(balances, c='red', label='GRID')
     plt.legend(loc='best')
